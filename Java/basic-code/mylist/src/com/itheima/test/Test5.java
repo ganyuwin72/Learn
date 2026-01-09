d598220c531133898dad88388291038206cad450
Yan Tong <ganyuwin72@gmail.com>
2026-01-09 17:46:54 +0800

    依旧ArrayList

---
+++ Java/basic-code/mylist/src/com/itheima/test/Test5.java
@@ -0,0 +1,14 @@
+package com.itheima.test;
+
+public class Test5 {
+    public static void main(String[] args) {
+        // 1.创建集合
+        ArrayList<Student> list = new ArrayList<>();
+
+        // 2.键盘录入学生信息并添加到集合中
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < 3; i++) {
+            
+        }
+    }
+}
